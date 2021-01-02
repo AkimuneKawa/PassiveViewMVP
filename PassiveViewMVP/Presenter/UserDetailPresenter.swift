b4a13b9c16e2b7a5c52de6a6f4c03a9aac576583
Kawa AKimune <akimune1003@gmail.com>
2021-01-03 02:38:56 +0900

    create UserDetailPresenter impl

--- PassiveViewMVP/Presenter/UserDetailPresenter.swift
+++ PassiveViewMVP/Presenter/UserDetailPresenter.swift
@@ -19,3 +19,42 @@ protocol UserDetailPresenterOutput: AnyObject {
     func updateRepositories(_ repositories: [Repository])
 }
 
+final class UserDetailPresenter: UserDetailPresenterInput {
+    private let userName: String
+    private var repositories: [Repository] = []
+    
+    private weak var view: UserDetailPresenterOutput!
+    private let model: UserDetailModelInput
+    
+    init(userName: String, view: UserDetailPresenterOutput, model: UserDetailModelInput) {
+        self.userName = userName
+        self.view = view
+        self.model = model
+    }
+    
+    var numberOfRepositories: Int {
+        return repositories.count
+    }
+    
+    func repository(forRow row: Int) -> Repository? {
+        guard row < repositories.count else { return nil }
+        return repositories[row]
+    }
+    
+    func viewDidLoad() {
+        model.fetchRepositories() { [weak self] result in
+            guard let self = self else { return }
+            switch result {
+            case .success(let repositories):
+                self.repositories = repositories
+                
+                DispatchQueue.main.async {
+                    self.view?.updateRepositories(repositories)
+                }
+            case .failure(let error):
+                ()
+                // TODO: error handling
+            }
+        }
+    }
+}
